8d1cf2cf2e9f18181be690aa77a96055873f623d
Alastair Pharo <alastair.pharo@blake.com.au>
2021-03-19 15:35:48 +1100

    Add "approve" function

--- index.js
+++ index.js
@@ -3,16 +3,39 @@
 const { Octokit } = require("@octokit/rest")
 const chalk = require('chalk')
 const contrast = require('contrast')
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const prompts = require('prompts')
 
 // https://github.com/settings/tokens
 const { DEPENDABOT_PRS_GITHUB_TOKEN, DEPENDABOT_PRS_REPOS = "" } = process.env
+const repos = DEPENDABOT_PRS_REPOS.split(/\s+/).map(repo => repo.trim())
 
 const owner = 'blake-education'
-const repos = DEPENDABOT_PRS_REPOS.split(/\s+/).map(repo => repo.trim())
 
 const octokit = new Octokit({
   auth: `token ${DEPENDABOT_PRS_GITHUB_TOKEN}`
 })
+
+function isDependabotPR({ labels }) {
+  return labels.some((label) => label.name === 'dependencies')
+}
+
+async function getPull(repo, number) {
+  const { data: { head: { sha }, labels, title, html_url: url, state } } = await octokit.pulls.get({
+    owner,
+    repo,
+    pull_number: number,
+  })
+
+  const { data: { state: status } } = await octokit.repos.getCombinedStatusForRef({
+    owner,
+    repo,
+    ref: sha,
+  })
+
+  return { repo, number, labels, title, url, state, status }
+}
 
 async function getPullsForRepo(repo) {
   const { data: pulls } = await octokit.pulls.list({
@@ -22,35 +45,113 @@ async function getPullsForRepo(repo) {
   })
 
   return (await Promise.all(pulls.map(async ({ head: { sha }, number, labels, title, html_url: url }) => {
-    if (!labels.find((label) => label.name === 'dependencies')) return
+    if (!isDependabotPR({ labels })) return
 
-    const { data: { state } } = await octokit.repos.getCombinedStatusForRef({
+    const { data: { state: status } } = await octokit.repos.getCombinedStatusForRef({
       owner,
       repo,
       ref: sha,
     })
 
-    return { repo, number, labels, title, url, state }
+    return { repo, number, labels, title, url, state: 'open', status }
   }))).flat().filter(Boolean)
 }
 
-;(async function () {
-  const pulls = (await Promise.all(repos.map(getPullsForRepo))).flat()
+async function getPullsForRepos() {
+  return (await Promise.all(repos.map(getPullsForRepo))).flat()
+}
 
-  for (const { repo, number, labels, title, url, state } of pulls) {
-    console.log(state)
-    const dotColor = {
-      error: 'red',
-      failure: 'red',
-      success: 'green',
-    }[state] ?? 'orange'
+function describePR({ repo, number, labels, title, url, status }) {
+  const dotColor = {
+    error: 'red',
+    failure: 'red',
+    success: 'green',
+  }[status] ?? 'orange'
 
-    const labelText = labels.map((label) => {
-      const bgColor = `#${label.color}`
-      const color = contrast(bgColor) === 'light' ? 'black' : 'white'
-      return chalk.bgHex(bgColor).keyword(color)(label.name)
-    }).join(' ')
+  const labelText = labels.map((label) => {
+    const bgColor = `#${label.color}`
+    const color = contrast(bgColor) === 'light' ? 'black' : 'white'
+    return chalk.bgHex(bgColor).keyword(color)(label.name)
+  }).join(' ')
 
-    console.log(`${chalk.blue(repo)}: ${chalk.magenta(`#${number}`)} ${chalk.bold(title)} ${chalk.keyword(dotColor)('•')} ${labelText} ${url}`)
+  return `${chalk.blue(repo)}: ${chalk.magenta(`#${number}`)} ${chalk.bold(title)} ${chalk.keyword(dotColor)('•')} ${labelText} ${url}`
+}
+
+async function listPRs() {
+  const pulls = await getPullsForRepos()
+
+  for (const pull of pulls) {
+    console.log(describePR(pull))
   }
-})()
+}
+
+async function approvePR({ repo, number }) {
+  const pull = await getPull(repo, number)
+  console.log(describePR(pull))
+
+  if (pull.state !== 'open') {
+    console.error(`#${number} is ${pull.state}!`)
+    return
+  }
+
+  if (!isDependabotPR(pull)) {
+    console.error(`#${number} is not a dependabot PR!`)
+    return
+  }
+
+  if (pull.status !== 'success') {
+    console.error(`#${number} has a failed check!`)
+    return
+  }
+
+  const { confirmation } = await prompts({
+    type: 'confirm',
+    name: 'confirmation',
+    message: 'Are you sure you want to approve this PR?',
+  })
+
+  if (!confirmation) return
+
+  await octokit.pulls.createReview({
+    owner,
+    repo,
+    pull_number: number,
+    commit_id: pull.sha,
+    event: 'APPROVE',
+  })
+
+  console.log(`#${number} approved`)
+}
+
+yargs(hideBin(process.argv))
+  .command('list', 'list PRs', () => {}, listPRs)
+  .command('approve <repo> <number>', 'approve a PR', {
+    repo: {
+      describe: 'name of a repo',
+      type: 'string',
+    },
+    number: {
+      describe: 'number of PR',
+      type: 'number',
+    }
+  }, (argv) => {
+    // Required to prevent the command running in completion mode
+    if ('getYargsCompletions' in argv) return
+    return approvePR(argv)
+  })
+  .completion('completion', 'output completion stuff', (_, { _: argv }) => {
+    if (argv[1] === 'approve' && argv.length > 2) {
+      if (argv.length === 3) {
+        return repos.map((repo) => `${repo}:`)
+      } else if (argv.length === 4) {
+        const repo = argv[2]
+        return getPullsForRepo(repo).then((pulls) => {
+          return pulls.map(({ number, title, status }) => `${number}:${title} (${status})`)
+        })
+      }
+    } else if (argv.length === 2) {
+      return ['list:list PRs', 'approve:approve a PR']
+    }
+  })
+  .help()
+  .argv
